789402fc547c36f1087cbdd9cbd54d74d2b2c9f8
zOok91 <zO_ok@zO-ok-2.local>
2019-07-29 22:04:37 +0530

    removing unused code

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import helmetImage from '../assetts/helmet.png';
-import axios from 'axios';
 import { auth } from '../utils';
 import './LoginScreen.css';
 import { userAuthentication } from '../apiCall';
-
 export default class LoginScreen extends Component {
   constructor(){
     super();
@@ -18,26 +16,12 @@ export default class LoginScreen extends Component {
     let {history} = this.props;
     let {username,password} = this.state;
     const userFound = await userAuthentication(username,password);
-    console.log('Usrefoudnd',userFound);
     if(userFound){
       auth.login();
       history.push('/search');
     }else{
       this.setState({errorFlag:true})    
     }
-    // await axios.post('/login', { username: this.state.username,
-    // password: this.state.password}, {}).then(function(response) {
-    //   if(response.data){
-    //     auth.login();
-    //     history.push('/search');
-    //   }else{
-    //    error=true;
-    //   }
-    // }).catch(function(error) {
-    //   console.log('Error on Authentication',error);
-    // });
-    // this.setState({errorFlag:error})
-
   }
   handleChange=(field,e)=>{
     field === 'user'?this.setState({username:e.target.value}):this.setState({password:e.target.value});
